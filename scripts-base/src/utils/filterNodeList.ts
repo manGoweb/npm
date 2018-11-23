1db60f9d73b9b2bc6d2f2b25ea82a9879de3a0ce
Jirka Vebr <jirkavebr@gmail.com>
2018-11-23 16:21:35 +0100

    Update filterNodeList

--- scripts-base/src/utils/filterNodeList.ts
+++ scripts-base/src/utils/filterNodeList.ts
@@ -1,4 +1,4 @@
 import { matchesSelector } from './matchesSelector'
 
-export const filterNodeList = <E extends HTMLElement>(list: NodeListOf<E>, selector: string): Array<E> =>
+export const filterNodeList = <E extends HTMLElement>(list: NodeListOf<E> | HTMLCollectionOf<E>, selector: string): Array<E> =>
 	Array.prototype.filter.call(list, (item: E) => matchesSelector(item, selector))
