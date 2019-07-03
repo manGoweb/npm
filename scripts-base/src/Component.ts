4396d37cda4cd1794f363b3fe34e01d5bd7272f5
Jirka Vebr <jirkavebr@gmail.com>
2019-07-03 10:55:48 +0200

    Component: verbose type parameters

--- scripts-base/src/Component.ts
+++ scripts-base/src/Component.ts
@@ -13,29 +13,32 @@ export interface NamedComponent {
 }
 
 export type ComponentConstructor<
-	D,
-	E extends ComponentEl,
-	EMap extends EventMap = EventMapByElement<E>
-> = NamedComponent & (new (element: E, data: D) => Component<D, E, EMap>)
+	Data,
+	ComponentElement extends ComponentEl,
+	EMap extends EventMap = EventMapByElement<ComponentElement>
+> = NamedComponent & (new (element: ComponentElement, data: Data) => Component<Data, ComponentElement, EMap>)
 
 export class ComponentInitializationError extends Error {}
 
-export class Component<D = {}, E extends ComponentEl = HTMLElement, EMap extends EventMap = EventMapByElement<E>> {
-	protected readonly getListeners = (): EventListeners<E, EMap> => []
+export class Component<
+	Data = {},
+	ComponentElement extends ComponentEl = HTMLElement,
+	EMap extends EventMap = EventMapByElement<ComponentElement>
+> {
+	protected readonly getListeners = (): EventListeners<ComponentElement, EMap> => []
 
-	constructor(protected readonly el: E, protected readonly data: D) {}
+	constructor(protected readonly el: ComponentElement, protected readonly data: Data) {}
 
 	public setup() {
 		this.attachListeners()
-
 		this.init()
 	}
 
-	protected getChild<C extends DelegateTarget<E>>(
+	protected getChild<Child extends DelegateTarget<ComponentElement>>(
 		selector: string,
-		ChildConstructor: new (...args: any[]) => C,
+		ChildConstructor: new (...args: any[]) => Child,
 		parent: HTMLElement | SVGElement = this.el as HTMLElement | SVGElement
-	): C {
+	): Child {
 		const child = parent.querySelector(selector)
 
 		if (!(child instanceof ChildConstructor)) {
@@ -46,12 +49,18 @@ export class Component<D = {}, E extends ComponentEl = HTMLElement, EMap extends
 		return child
 	}
 
-	protected getChildren<C extends DelegateTarget<E>>(selector: string, parent = this.el as HTMLElement | SVGElement) {
+	protected getChildren<C extends DelegateTarget<ComponentElement>>(
+		selector: string,
+		parent = this.el as HTMLElement | SVGElement
+	) {
 		return parent.querySelectorAll(selector) as NodeListOf<C>
 	}
 
-	protected getProp<N extends keyof D>(prop: N, defaultValue: Exclude<D[N], undefined>): Exclude<D[N], undefined> {
-		return this.data[prop] === undefined ? defaultValue : (this.data[prop] as Exclude<D[N], undefined>)
+	protected getProp<N extends keyof Data>(
+		prop: N,
+		defaultValue: Exclude<Data[N], undefined>
+	): Exclude<Data[N], undefined> {
+		return this.data[prop] === undefined ? defaultValue : (this.data[prop] as Exclude<Data[N], undefined>)
 	}
 
 	public init() {}
@@ -79,8 +88,8 @@ export class Component<D = {}, E extends ComponentEl = HTMLElement, EMap extends
 
 						while (target && target !== this.el && target instanceof Element) {
 							if (matchesSelector(target, delegateSelector)) {
-								const delegateEvent = (e as unknown) as DelegateEvent<typeof type, E, EMap>
-								delegateEvent.delegateTarget = target as DelegateTarget<E>
+								const delegateEvent = (e as unknown) as DelegateEvent<typeof type, ComponentElement, EMap>
+								delegateEvent.delegateTarget = target as DelegateTarget<ComponentElement>
 
 								return callback.call(this, delegateEvent)
 							}
