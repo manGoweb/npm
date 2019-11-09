3b11ee2bc141f8e455eeb6451197fa12b75e6c9b
Jirka Vebr <jirkavebr@gmail.com>
2019-11-09 19:22:22 +0100

    Prettier: fix syntax

--- scripts-base/src/Component.ts
+++ scripts-base/src/Component.ts
@@ -69,6 +69,7 @@ export class Component<
 			: (this.props[propName] as Exclude<Props[PropName], undefined>)
 	}
 
+	// eslint-disable-next-line @typescript-eslint/no-empty-function
 	public init() {}
 
 	private attachListeners() {
