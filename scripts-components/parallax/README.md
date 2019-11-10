adcac42c43554593c1e7e5889bf25e0ef1fa3b89
Filip Chalupa <chalupa.filip@gmail.com>
2019-11-10 23:50:46 +0100

    Parallax: 0.0.1

---
+++ scripts-components/parallax/README.md
@@ -0,0 +1,34 @@
+# InView
+
+The manGoweb Parallax component. Watches elements vertical position in viewport.
+
+## Installation
+
+`$ npm install @mangoweb/parallax @mangoweb/scripts-base`
+
+This is a [`@mangoweb/scripts-base`](https://www.npmjs.com/package/@mangoweb/scripts-base) component.
+
+## Usage
+
+In your template:
+
+```html
+<script>
+	window.initComponents = (window.initComponents || []).push({
+		name: 'Parallax',
+		place: '#targetElement',
+		data: {
+			// Optional
+			customProperty: 'position', // Custom property name
+		},
+	})
+</script>
+```
+
+In your styles
+
+```css
+#targetElement {
+	transform: translateY(calc(var(--position, 0) * 20vh)); // Translates by 0 pixels when javascript is disabled
+}
+```
---
+++ scripts-components/parallax/src/index.ts
@@ -0,0 +1,33 @@
+import { Component } from '@mangoweb/scripts-base'
+
+interface ParallaxProps {
+	customProperty?: string
+}
+
+const PARALLAX_REACH = 1 // 100vh
+
+function clamp(min: number, value: number, max: number) {
+	return Math.min(Math.max(value, min), max)
+}
+
+export class Parallax extends Component<ParallaxProps> {
+	public static componentName = 'Parallax'
+
+	init() {
+		window.addEventListener('scroll', this.onScroll, { passive: true })
+		this.onScroll()
+	}
+
+	onScroll = () => {
+		const reachDistance = window.innerHeight * PARALLAX_REACH
+		const offset = clamp(-1, (this.getElementYCenter() - (window.scrollY + window.innerHeight / 2)) / reachDistance, 1)
+
+		this.el.style.setProperty(`--${this.getPropOrElse('customProperty', 'parallax')}`, `${offset}`)
+	}
+
+	getElementYCenter = () => {
+		// @TODO: debounce
+		const rect = this.el.getBoundingClientRect()
+		return window.scrollY + rect.top + rect.height / 2
+	}
+}
